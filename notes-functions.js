7f7f438cd3f3ca120c5fdca4359adad3e778722d
Kyle Berry <kyleberry@Kyles-MacBook-Pro.local>
2018-12-14 17:09:19 -0700

    adds completed refactored code

--- notes-functions.js
+++ notes-functions.js
@@ -1,7 +1,6 @@
 // Refactoring the notes-app-one and making a seperate file for all the functions in script.js
 
 // This function reads existing notes from localStorage
-
 const getSavedNotes = function(){
     const notesJSON = localStorage.getItem('notes');
     if(notesJSON !== null) {
@@ -11,3 +10,26 @@ const getSavedNotes = function(){
         return [];
     }
 }
+
+// Generate the DOM structure for a note
+const generateNoteDOM = function(note){
+    const noteEl = document.createElement('p')           
+    if(note.title.length > 0){
+        noteEl.textContent = note.title;
+    } else {
+        noteEl.textContent = 'Unamed note';
+    }
+    return noteEl;
+}
+
+// Render application notes 
+const renderNotes = function(notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+    document.querySelector('#notes').innerHTML = '';  
+    filteredNotes.forEach(function(note){
+        const noteEl = generateNoteDOM(note);
+        document.querySelector('#notes').appendChild(noteEl);
+    })
+}  
--- script.js
+++ script.js
@@ -1,37 +1,8 @@
-
-// All of the notes in the notes obeject that we render on the screen using the renderNotes function.
-
 const notes = getSavedNotes();
 
 const filters = {
     searchText: ''
-}
-
-const renderNotes = function(notes, filters){
-
-    const filteredNotes = notes.filter(function(note){
-
-
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-    })
-
-    document.querySelector('#notes').innerHTML = '';
-    
-    filteredNotes.forEach(function(note){
-        const noteEl = document.createElement('p')
-            
-        if(note.title.length > 0){
-            noteEl.textContent = note.title;
-        } else {
-            noteEl.textContent = 'Unamed note';
-        }
-
-
-        document.querySelector('#notes').appendChild(noteEl);
-    })
-}   
-
-// executing the renderNotes function;
+};
 
 renderNotes(notes, filters);
 
@@ -42,21 +13,16 @@ document.querySelector('#create-note').addEventListener('click', function(e){
     })
     localStorage.setItem('notes', JSON.stringify(notes))
     renderNotes(notes, filters)
-})
-
-
-// Adds the input event that listens for a change everytime we type a new character.
-// When we add a new character the renderNotes function runs
-// Whatever the search box input is we set the value of the search text in the filters object to that value.
+});
 
 document.querySelector('#search-text').addEventListener('input', function(e){
     filters.searchText = e.target.value;
     renderNotes(notes, filters);
-})
+});
 
 document.querySelector('#filter-by').addEventListener('change', function(e){
     console.log(e.target.value);
-})
+});
 
 
 
